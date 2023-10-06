fb5c78e1c246243313ebf4e7e0fab55b400ca662
TimelessOn15Hz <timeless.off.tv@gmail.com>
2023-10-07 01:42:02 +0300

    Update fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -2,7 +2,8 @@
 import discord
 import random
 import aiohttp
-import asyncio
+import time
+from components import lists
 
 #FROM DISCORD
 from discord import app_commands, ui
@@ -20,8 +21,9 @@ class Fun(commands.Cog):
 
 
     #MEME COMMAND
-    @app_commands.command(name="halloween", description="Halloween reddit posts!")
-    async def halloween(self, interaction: discord.Interaction) -> None:
+    @app_commands.command(name="meme", description="Spooky memes just for you! This command doesn't work sometimes, sorry :(")
+    @app_commands.checks.cooldown(1, 5.0)
+    async def meme(self, interaction: discord.Interaction) -> None:
         subreddit = lists.halloweensub
 
         async with aiohttp.ClientSession() as clientsesh:
@@ -32,23 +34,23 @@ class Fun(commands.Cog):
                 image_url = random_post["data"]["url"]
                 title = random_post["data"]["title"]
                 subreddit_title = random_post["data"]["subreddit"]
-                post_link = random_post["data"]["permalink"]
 
-
-                embed = discord.Embed(
-                    title=f"{title}",
-                    description=f"[Link](https://reddit.com{post_link})", 
-                    color=0xEB6123
-                )
+                embed = discord.Embed(title=f"{title}", description="", color=0xb40000)
                 embed.set_image(url=image_url)
-                embed.set_footer(text=f"By r/{subreddit_title}") 
+                embed.set_footer(text=f"By r/{subreddit_title}")
                 await interaction.response.send_message(embed=embed)
 
-
-    @halloween.error
-    async def clear_error(self, interaction: discord.Interaction, error: AppCommandError):
-        if isinstance(error, app_commands.CommandInvokeError):
-                await interaction.response.send_message("Something went wrong when fetching spooky memes! Try running the command again!", ephemeral=True)
+    @meme.error
+    async def meme_error(self, interaction: discord.Interaction, error: AppCommandError) -> None:
+        if isinstance(error, app_commands.CommandOnCooldown):
+            unixtime = int(time.time())
+            totaltime = unixtime + int(error.retry_after)
+            embed = discord.Embed(
+                title="Slow down!",
+                description=f"You can use this command again <t:{totaltime}:R>",
+                color=0xb40000
+            )
+            await interaction.response.send_message(embed=embed, ephemeral=True)
 
 
     #SPOOKIFY COMMAND
@@ -84,7 +86,9 @@ class Fun(commands.Cog):
     @app_commands.choices(story = [
         Choice(name = "Story 1 - The Kidnapping", value = "story1"),
         Choice(name = "Story 2 - A Spooky Encounter", value = "story2"),
-        Choice(name = "Story 3 - Trick or Treat", value = "story3")
+        Choice(name = "Story 3 - Trick or Treat", value = "story3"),
+        Choice(name = "Story 4 - COMING SOON", value = "story4"),
+        Choice(name = "Story 4 - COMING SOON", value = "story5")
     ])
     async def spookystory(self, interaction: discord.Interaction, story: str):
         if story == "story1":
@@ -93,6 +97,10 @@ class Fun(commands.Cog):
             await interaction.response.send_modal(SpookyStory2())
         elif story == "story3":
             await interaction.response.send_modal(SpookyStory3())
+        elif story == "story4":
+            await interaction.response.send_modal(SpookyStory4())
+        elif story == "story5":
+            await interaction.response.send_modal(SpookyStory5())
 
 
 class SpookyStory1(ui.Modal, title="Spooky Story Generator!"):
@@ -104,7 +112,14 @@ class SpookyStory1(ui.Modal, title="Spooky Story Generator!"):
 
     async def on_submit(self, interaction: discord.Interaction):
         await interaction.response.send_message(
-            f"Once upon a time...\n\nThere was __{self.name}__, chilling on the couch, watching spooky memes on the phone, and sipping on __{self.drink}__.\nAnd, since it's spooky month, and it's the best month of the year (obviously), __{self.name}__ decided to do something different this time around.\n__{self.name}__ decided to *kidnap a stranger* and *tie him on a chair* in the basement.\n__{self.name}__ then put __{self.music}__ music on max volume and left.\nThe stranger felt __{self.emotion}__ while being tortured by the loud music.\nThen the stranger got spooked by a spooky ghost and died.\n\nTHE END."
+            f"# Once upon a time...\n\n\
+            There was __{self.name}__, chilling on the couch, watching spooky memes on the phone, and sipping on __{self.drink}__.\n\
+            And, since it's spooky month, and it's the best month of the year (obviously), __{self.name}__ decided to do something different this time around.\n\
+            So, __{self.name}__ decided to ***kidnap a stranger*** and ***tie him on a chair*** in the basement. Perfectly normal for spooky month.\n\
+            __{self.name}__ then played __{self.music}__ on max volume and left.\n\
+            The stranger felt __{self.emotion}__ while being tortured by the loud music.\n\
+            Then the stranger got spooked by a spooky ghost and died.\n\n\
+            ## THE END."
             )
 
         
@@ -118,23 +133,63 @@ class SpookyStory2(ui.Modal, title="Spooky Story Generator!"):
 
     async def on_submit(self, interaction: discord.Interaction):
         await interaction.response.send_message(
-            f"Once upon a time...\n\nThere was __{self.name}__ and their huge mansion! The mansion had all sorts of rooms... A theater, a bar, and a lot of jacuzzis!?\nOne day, while __{self.name}__ was relaxing in jacuzzi #__{self.number}__, a spooky ghost suddenly appeared in front of them!\nThey stared at eachother very awkwardly, until the ghost started speaking in its deep voice...\n'Hello stranger... I would like to say something...'\n__{self.name}__, being extremely confused, agreed to hear what the ghost had to say.\n'__{self.say}__', said the ghost.\n'What?', __{self.name}__ responded.\nThe ghost then started __{self.verb}__.\n__{self.name}__ felt __{self.emotion}__ with what the ghost said...\nThen __{self.name}__ got jumpscared by their spooky pet spider and died.\n\nTHE END."
+            f"# Once upon a time...\n\n\
+            There was __{self.name}__ and their huge mansion! The mansion had all sorts of rooms... A theater, a bar, and a lot of jacuzzis!\n\
+            One day, while __{self.name}__ was relaxing in jacuzzi #__{self.number}__, a spooky ghost suddenly appeared in front of them!\n\
+            __{self.name}__ and the ghost stared at eachother very awkwardly, until the ghost started speaking in its deep, scary voice:\n\
+            'Hello stranger... I would like to say something...'\n\
+            __{self.name}__, being extremely confused, agreed to hear what the ghost had to say.\n\
+            '__{self.say}__', said the ghost.\n\
+            'What?', __{self.name}__ responded.\n\
+            The ghost then started __{self.verb}__.\n\
+            __{self.name}__ felt __{self.emotion}__ with what the ghost had to say.\n\
+            Then __{self.name}__ got jumpscared by their spooky pet spider and died.\n\n\
+            ## THE END."
             )
 
 
 class SpookyStory3(ui.Modal, title="Spooky Story Generator!"):
 
     outfit1 = ui.TextInput(label="Costume 1", style=discord.TextStyle.short, placeholder="Name a costume! (e.g. 'a ghost costume')", required=True)
-    outfit2 = ui.TextInput(label="Costume 2", style=discord.TextStyle.short, placeholder="Name a costume! (e.g. 'a zombie costume')", required=True)
+    outfit2 = ui.TextInput(label="Costume 2", style=discord.TextStyle.short, placeholder="Name a second costume! (e.g. 'a zombie costume')", required=True)
     treat = ui.TextInput(label="Treat", style=discord.TextStyle.short, placeholder="Your favourite treat! (e.g. candy, chocolate)", required=True)
     feeling = ui.TextInput(label="Feeling", style=discord.TextStyle.short, placeholder="Give a feeling! (e.g. shocked, confused)", required=True)
     neighbour = ui.TextInput(label="Neighbour", style=discord.TextStyle.short, placeholder="Name a neighbour! (e.g. grandma, teacher)", required=True)
 
     async def on_submit(self, interaction: discord.Interaction):
-        spooktober = "IT'S SPOOKTOBER!"
         await interaction.response.send_message(
-            f"Once upon a time...\n\nThere were two bros chillin' in a hot tub, five feet apart cuz they're not gay (haha get it)\nThey were chatting about how spooktober is arriving soon, and what they should do.\nSuddenly, they heard bells going off...they were both __{self.feeling}__. After a short pause, they both said to each other with excitement in their eyes...\n'{spooktober}'\nSo they got out of the hot tub and quickly went inside to change to their spooky costumes!\nThe first outfit was __{self.outfit1}__! Very spooky.\nThe second outfit was __{self.outfit2}__! Even spookier!\nThey then got their spooky bags and went on their way to Trick or Treat!\nTheir favourite treat was __{self.treat}__! That's a really tasty treat!\nThe first house they went to was their neighbour __{self.neighbour}__'s house!\nThey knocked on the door and the __{self.neighbour}__ slowly opened the door...\n'TRICK OR TREAT?', the bros shouted.\nThen the __{self.neighbour}__ got a heart attack and almost died but the ambulance arrived in time.\n(you really thought someone was going to die again, shame on you)\n\nTHE END."
+            f"# Once upon a time...\n\n\
+            There were two friends chillin' in their very expensive jacuzzi. (these stories have way too many for whatever reason)\n\
+            They were chatting about how spooktober is arriving soon, and what they should do.\n\
+            Suddenly, they heard their phones go off...they were both __{self.feeling}__. After a short pause, they both said to each other with excitement in their eyes...\n\
+            '**IT'S SPOOKTOBER!!!**'\n\
+            So they got out of the jacuzzi and quickly went inside to change to their spooky costumes!\n\
+            The first outfit was __{self.outfit1}__! Very spooky.\n\
+            The second outfit was __{self.outfit2}__! Even spookier!\n\
+            They then got their snack bags and went on their way to Trick or Treat!\n\
+            Their favourite treat was __{self.treat}__! That's a really tasty treat!\n\
+            The first house they went to was their neighbour __{self.neighbour}__'s house!\n\
+            They knocked on the door and the door opened slowly...\n\
+            'TRICK OR TREAT?', the bros shouted.\n\
+            Then the __{self.neighbour}__ got a heart attack and almost died but the ambulance arrived in time.\n\
+            (you really thought someone was going to die again, shame on you)\n\n\
+            ## THE END."
             )
+
+class SpookyStory4(ui.Modal, title="Spooky Story Generator!"):
+
+    variable = ui.TextInput(label="Costume 1", style=discord.TextStyle.short, placeholder="Name a costume! (e.g. 'a ghost costume')", required=True)
+
+    async def on_submit(self, interaction: discord.Interaction):
+        await interaction.response.send_message("COMING SOON")
+           
+
+class SpookyStory5(ui.Modal, title="Spooky Story Generator!"):
+
+    variable = ui.TextInput(label="Costume 1", style=discord.TextStyle.short, placeholder="Name a costume! (e.g. 'a ghost costume')", required=True)
+
+    async def on_submit(self, interaction: discord.Interaction):
+        await interaction.response.send_message("COMING SOON!")
 
 
 async def setup(bot):
